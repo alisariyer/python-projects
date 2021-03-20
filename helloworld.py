f7f8b92c0fb895d9f3e48cc6a6b82cefe3c74bcd
alisariyer <77790547+alisariyer@users.noreply.github.com>
2021-03-20 10:51:55 +0100

    Add actual date with datetime module

--- helloworld.py
+++ helloworld.py
@@ -2,8 +2,12 @@
 Let's start projects
 by Ali Sariyer
 """
+from datetime import datetime
+
 def main():
     print("Hello World")
+    print("Today is " + str(datetime.now()))
+    
 
 if __name__ == "__main__":
     main()
